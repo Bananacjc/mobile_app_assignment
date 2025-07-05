47468ad133bafee4d029c1ce040be9a0f18f6085
Bananacjc <jauchun040322@gmail.com>
2025-07-06 02:40:22 +0800

    Added app_colors.dart to set default colors, can add more later

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color softWhite = Color(0xFFEFEFEF);
+  static const Color slateGray = Color(0xFF585858);
+  static const Color darkCharcoal = Color(0xFF282A38);
+  static const Color royalViolet = Color(0xFF6A54E4);
+  static const Color accentMint = Color(0xFF29A87A);
+  static const Color primaryGreen = Color(0xFF218662);
+}
